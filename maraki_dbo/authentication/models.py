f66849e15e816d839e2c5e0a9a0aad3b5abe8ba9
Yonathan Assefa <yab95assefa@gmail.com>
2024-03-11 11:11:51 +0300

    app name renamed from auth to atuthentication

--- maraki_dbo/authentication/models.py
+++ maraki_dbo/authentication/models.py
@@ -24,10 +24,24 @@ class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(unique=True)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
+    groups = models.ManyToManyField(
+        'auth.Group',
+        related_name='auth_user_set',  # Added related_name
+        blank=True,
+        help_text='The groups this user belongs to.',
+        verbose_name='groups',
+    )
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        related_name='auth_user_set',  # Added related_name
+        blank=True,
+        help_text='Specific permissions for this user.',
+        verbose_name='user permissions',
+    )
 
     objects = UserManager()
 
-    USERNAME_FIELD = "email"
+    USERNAME_FIELD = 'email'
 
     def __str__(self):
         return self.email
