7120368e04b86eb35c8b49b688ce6ff65dea4c95
Yonathan Assefa <yab95assefa@gmail.com>
2024-03-11 23:19:05 +0300

    profession model added to the core model
    This is the last update i made before i create my first new card_manager / card application

--- maraki_dbo/core/models.py
+++ maraki_dbo/core/models.py
@@ -22,7 +22,7 @@ class Title(models.Model):
 
 class EducationLevel(models.Model):
     title = models.CharField(max_length = 255, unique = True)
-    
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     phone_number = models.CharField(max_length=13, blank=True)
@@ -35,3 +35,11 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.user.username
+    
+
+# Profession Model
+class Profession(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    icon = models.ImageField(upload_to="profession_icons/", blank=True, null=True)
+
+
